a9ac3111125ec8b114d5329d15e5206e7a1bce28
soO <444deph12@gmail.com>
2023-06-16 12:14:08 -0700

    forgot area on followcircle

--- src/objects/BowWeapon.js
+++ src/objects/BowWeapon.js
@@ -115,6 +115,7 @@ export function monsterBow(monster) {
             k.circle(8), // Set the size of the circle
             k.follow(flyingArrow, vec2(0, 0)), // Make the circle follow the flying arrow
             k.opacity(0.2),
+            k.area(),
             k.outview({ destroy: true }),
             "followCircle",
           ]);
