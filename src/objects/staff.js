b0d3e37de4ac366647bc1ffa72822e41f470a869
soO <444deph12@gmail.com>
2023-06-21 04:24:31 -0700

    changed the way the default attack work

--- src/objects/staff.js
+++ src/objects/staff.js
@@ -11,7 +11,7 @@ export function staff() {
   let playerDirection = "right";
   let canThrust = true;
   let thrustState = "idle";
-
+  let movingSpikeExists = false;
   const staff = k.add([
     k.pos(player.pos.x, player.pos.y),
     k.sprite("staff"),
@@ -80,82 +80,87 @@ export function staff() {
               thrustState = "thrusting";
             }
             break;
-
           case "thrusting":
             let distanceChange = (thrustDistance / thrustDuration) * k.dt();
             this.pos.x +=
               attackingDirection === "right" ? distanceChange : -distanceChange;
             if (actionProgress >= thrustDuration) {
-              actionProgress = 0;
+                actionProgress = 0;
               thrustState = "lowering";
-              const stationarySpike = k.add([
-                k.sprite("iceSpike", { anim: "spike" }),
-                k.pos(this.pos.x, this.pos.y - 24),
-                k.origin("center"),
-                k.rotate(0),
-                k.scale(0.4),
-                k.opacity(0.5),
-                k.lifespan(0.4),
-                "spell",
-              ]);
-              if (attackingDirection === "left") {
-                stationarySpike.flipX(true);
-              }
-              k.wait(0.4, () => {
-                const movingSpike = k.add([
-                  k.sprite("iceSpike", { anim: "spike" }),
-                  k.pos(stationarySpike.pos.x, stationarySpike.pos.y),
-                  k.origin("center"),
-                  k.rotate(0),
-                  k.area({ width: 12, height: 5 }),
-                  k.outview({ destroy: true }),
-                  attackingDirection === "right"
-                    ? k.move(0, 200)
-                    : k.move(0, -200),
-                  k.scale(0.4),
-                  {
-                    hitMonsters: new Set(),
-                    damage: 10,
-                  },
-                  "spelld",
-                ]);
-                if (attackingDirection === "left") {
-                  movingSpike.flipX(true);
-                }
-
-                movingSpike.play("spike");
-                k.every("enemy", (enemy) => {
-                  k.onCollide("spelld", "enemy", (s, e) => {
-                    if (e.dead != true) {
-                      if (!s.hitMonsters.has(e)) {
-                        e.hurt(s.damage);
-                        s.hitMonsters.add(e);
-                        const damageText = k.add([
-                          k.text(`-${s.damage}`, {
-                            size: 8, // text size
-                            font: "sinko",
-                          }),
-                          k.pos(e.pos.x, e.pos.y - 20), // Adjusted position to be above the monster
-                          k.lifespan(1),
-                          k.color(0, 100, 100),
-                          {
-                            value: -s.damage,
-                          },
-                        ]);
-                        damageText.onUpdate(() => {
-                          damageText.move(0, -40 * k.dt());
-                        });
-                        // Reduce the damage after each hit.
-                        s.damage -= 2;
-                        // If the damage is zero or less, destroy the moving spike.
-                        if (s.damage <= 0) {
-                          k.destroy(s);
-                        }
-                      }
+                  if (!movingSpikeExists) {
+                    // check if a moving spike already exists
+                    movingSpikeExists = true;
+                    const stationarySpike = k.add([
+                      k.sprite("iceSpike", { anim: "spike" }),
+                      k.pos(this.pos.x, this.pos.y - 24),
+                      k.origin("center"),
+                      k.rotate(0),
+                      k.scale(1),
+                      k.opacity(0.5),
+                      k.lifespan(0.4),
+                      "spell",
+                    ]);
+                    if (attackingDirection === "left") {
+                      stationarySpike.flipX(true);
                     }
-                  });
-                });
-              });
+                    k.wait(0.4, () => {
+                      const movingSpike = k.add([
+                        k.sprite("iceSpike", { anim: "spike" }),
+                        k.pos(stationarySpike.pos.x, stationarySpike.pos.y),
+                        k.origin("center"),
+                        k.rotate(0),
+                        k.area({ width: 12, height: 5 }),
+                        k.outview({ destroy: true,}),
+                        attackingDirection === "right"
+                          ? k.move(0, 350)
+                          : k.move(0, -350),
+                        k.scale(1),
+                        {
+                          hitMonsters: new Set(),
+                          damage: 30,
+                        },
+                        "spelld",
+                      ]);
+                      if (attackingDirection === "left") {
+                        movingSpike.flipX(true);
+                      }
+                      movingSpike.on("destroy", () => {
+                        movingSpikeExists = false; // Reset the flag when the moving spike is destroyed
+                      });
+
+                      movingSpike.play("spike");
+                      k.every("enemy", (enemy) => {
+                        k.onCollide("spelld", "enemy", (s, e) => {
+                          if (e.dead != true) {
+                            if (!s.hitMonsters.has(e)) {
+                              e.hurt(s.damage);
+                              s.hitMonsters.add(e);
+                              const damageText = k.add([
+                                k.text(`-${s.damage}`, {
+                                  size: 8,
+                                  font: "sinko",
+                                }),
+                                k.pos(e.pos.x, e.pos.y - 20),
+                                k.lifespan(1),
+                                k.color(0, 100, 100),
+                                {
+                                  value: -s.damage,
+                                },
+                              ]);
+                              damageText.onUpdate(() => {
+                                damageText.move(0, -40 * k.dt());
+                              });
+                              s.damage -= 5;
+                              if (s.damage <= 0) {
+                                k.destroy(s);
+                                movingSpikeExists = false;
+                              }
+                            }
+                          }
+                        });
+                      });
+                    });
+                  }
             }
             break;
 
@@ -165,7 +170,7 @@ export function staff() {
                 ? 90 - 90 * (actionProgress / raiseLowerDuration)
                 : 270 + 90 * (actionProgress / raiseLowerDuration);
             if (actionProgress >= raiseLowerDuration) {
-              this.angle = attackingDirection === "right" ? 0 : 0; // Force the angle to be exactly 0 (or 180)
+              this.angle = attackingDirection === "right" ? 0 : 0;
               actionProgress = 0;
               thrustState = "idle";
               canThrust = true;
